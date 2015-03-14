ec2b6a7ed11c893eaccca0621fbe9376e928c789
Jan Schulte <hello@unexpected-co.de>
2015-03-15 00:26:57 +0100

    if user is existant, return it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   def self.find_or_create_from_auth_hash(auth)
+    user = self.find_by_provider_and_uid(auth["provider"], auth["uid"])
+    return user unless user.nil?
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
