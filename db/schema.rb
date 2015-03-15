f352abbb11290d604cf3790cd20ec4507d028f09
Jan Schulte <hello@unexpected-co.de>
2015-03-15 20:44:14 +0100

    Add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314234651) do
+ActiveRecord::Schema.define(version: 20150315194359) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "languages", force: :cascade do |t|
     t.string   "name"
